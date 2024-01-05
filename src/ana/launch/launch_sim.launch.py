4d7254fe374c78f7a6b222642c22e724455866d4
Aidan Reilly <aidanr86@gmail.com>
2024-01-05 15:06:12 -0500

    added launch file to shorten commands

---
+++ src/ana/launch/launch_sim.launch.py
@@ -0,0 +1,42 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    package_name='ana'
+    
+    # below automatically does the sim_time, gazebo_ros, robot_description, and joint_state_publisher terminal commands
+
+    rsp = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name),'launch','rsp.launch.py'
+                )]), launch_arguments={'use_sim_time': 'true'}.items()
+    )
+
+    gazebo = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]), 
+    )
+
+    spawn_entity = Node(
+        package='gazebo_ros', executable='spawn_entity.py', 
+        arguments=['-topic', 'robot_description', '-entity', 'my_bot'],
+        output='screen')
+    
+    joint_state_publisher = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher'
+    )
+
+    # Launch them all!
+    return LaunchDescription([
+        rsp,
+        gazebo,
+        spawn_entity,
+        joint_state_publisher,
+    ])
