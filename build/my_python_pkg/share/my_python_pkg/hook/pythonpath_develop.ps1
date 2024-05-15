0d83241e387cf32d405ad3ac55717f918a714e0e
Aidan <aidanr86@gmail.com>
2024-05-15 17:37:00 -0400

    delete build files

--- build/my_python_pkg/share/my_python_pkg/hook/pythonpath_develop.ps1
+++
@@ -1,3 +0,0 @@
-# generated from colcon_powershell/shell/template/hook_prepend_value.ps1.em
-
-colcon_prepend_unique_value PYTHONPATH "$env:COLCON_CURRENT_PREFIX\/home/aidan/ana_bot/build/my_python_pkg"
